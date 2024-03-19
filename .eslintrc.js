41c486fde54c6f93b1c760b609f62e0a66e1b22f
Hernán Valencia <hernanadrianv17@gmail.com>
2024-03-19 11:29:10 -0500

    ♻️ refactor: configuring eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+const config = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['./tsconfig.json', './.eslintrc.js'], // Include .eslintrc.js in the project
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'next/core-web-vitals',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+    ],
+    rules: {
+        // These opinionated rules are enabled in stylistic-type-checked above.
+        // Feel free to reconfigure them to your own preference.
+        '@typescript-eslint/array-type': 'off',
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/prefer-for-of': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+        '@typescript-eslint/consistent-type-imports': [
+            'warn',
+            {
+                prefer: 'type-imports',
+                fixStyle: 'inline-type-imports',
+            },
+        ],
+        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+}
+
+module.exports = config
